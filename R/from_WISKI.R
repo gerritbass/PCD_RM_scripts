6bd9d45d769cdb6ee4bb74bf50f9fb4afc9fdbd9
Gerrit Bass <gerritb@palouseconservation.local>
2021-12-31 16:16:00 -0800

    OG commit

---
+++ R/from_WISKI.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(lubridate)
+
+
+WISKI_test <-  read_delim('P:/Research_and_Monitoring/_04_Project_Data/from_WISKI/test.zrx',skip = 5,comment = '#',col_names = FALSE,na = '-99999.0') %>%  
+               mutate(X1 = ymd_hms(X1))
+                      
+colnames(WISKI_test) <- c('DATE','temp_C')                      
+                
+ggplot(WISKI_test, aes(x=DATE, y=temp_C))+
+  geom_line()+
+  geom_point()
+
+
