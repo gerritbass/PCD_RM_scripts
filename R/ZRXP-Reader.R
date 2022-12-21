1d4693685608e3621358b1d214b368d0361c1984
Gerrit Bass <gerritb@palouseconservation.local>
2022-12-21 12:33:18 -0800

    not needed

--- R/ZRXP-Reader.R
+++
@@ -1,8 +0,0 @@
-# ZRXP Managing Script - For WISKI Exports 
-
-# KAM-THORN
-  KTZRXP = read.delim2(file = "P:/Research_and_Monitoring/_04_Project_Data/Water_Quality/_4_Exported_Data/Kam-Thorn/All-Data.ZRX", 
-                       header=FALSE, sep=" ", blank.lines.skip = T)
-  
-  
-  
